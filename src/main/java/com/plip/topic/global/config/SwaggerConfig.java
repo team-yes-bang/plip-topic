ba8a5a8909066fc77034535a4a23e6bca0ee1573
headdrop <ehdgmltn@gmail.com>
2026-08-22 17:36:23 +0900

    Chore: swagger 에 인증 UI추가

--- src/main/java/com/plip/topic/global/config/SwaggerConfig.java
+++ src/main/java/com/plip/topic/global/config/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.plip.topic.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+	private static final String BEARER_AUTH = "bearerAuth";
 
 	@Bean
 	public OpenAPI openAPI() {
@@ -14,6 +19,14 @@ public class SwaggerConfig {
 				.info(new Info()
 						.title("Topic Service API")
 						.description("아지트 토픽 API")
-						.version("v1.0.0"));
+						.version("v1.0.0"))
+				.components(new Components().addSecuritySchemes(
+						BEARER_AUTH,
+						new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")
+				))
+				.addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
 	}
 }
